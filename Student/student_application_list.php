81132632ff4a8b46443e09477f6a270f26dc5665
adamm <adamyaacob7@gmail.com>
2023-06-23 19:02:07 +0800

    added student application form and list

---
+++ Student/student_application_list.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application List</title>
+    <!-- Bootstrap Link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <div class="container my-5">
+        <h2>Full Application List</h2>
+        <p><i>Full List Only Accessible to Admins and Coordinators</i></p>
+        <!-- <a class="btn btn-primary" href="user_form.php" role="button">Add User</a> -->
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Application ID</th>
+                    <th>Applicant</th>
+                    <th>Date Applied</th>
+                    <th>Title</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+
+                session_start();
+
+                // Retrieve data for the logged-in student
+                $studentId = $_SESSION['USER_ID']; 
+
+                  // Check if the student ID is set
+                  if (isset($studentId)) {
+                    require_once("../config.php");
+                    require_once("../functions.php");
+
+                    // Retrieve data from the table
+                    $array = array();
+                    $select = "SELECT * FROM practical_training WHERE fk_userid = $studentId";
+                    $sql = mysqli_query($GLOBALS['conn'], $select);
+
+                    // Check if the query executed successfully
+                    if ($sql) {
+                        if (mysqli_num_rows($sql) > 0) {
+                            // Output data of each row
+                            while ($row = mysqli_fetch_array($sql)) {
+                              $array['userid'] = $row['fk_userid'];
+                              $profile = getUsersData($array['userid']);
+                              echo "
+                              <tr>
+                                  <td>$row[applicationid]</td>
+                                  <td>$profile[name]</td>
+                                  <td>$row[applicationdate]</td>  
+                                  <td>$row[applicationtitle]</td>
+                                  <td>$row[applicationstatus]</td>
+                                  <td>
+                                      <a class='btn btn-primary btn-sm' href='edit_user_form.php?id=$row[applicationid]'>Edit</a>
+                                      <a class='btn btn-danger btn-sm' href='delete_user.php?id=$row[applicationid]'>Delete</a>
+                                      <a class='btn btn-dark btn-sm' href='view_user.php?id=$row[applicationid]'>View</a>
+                                  </td>
+                              </tr> 
+                              ";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+
+                        mysqli_close($conn);
+                    } else {
+                        echo "Query execution failed.";
+                    }
+                  } else {
+                    echo "Student ID is not set.";
+                  }
+
+                // // Retrieve data from table
+                // $array = array();
+                // $studentId = $_SESSION['studentId']; // Assuming you have stored the student ID in the session variable
+
+                // $select = "SELECT * FROM practical_training WHERE fk_userid = $studentId";
+
+                // if (mysqli_num_rows($sql) > 0) {
+                //     // Output data of each row
+                //     while ($row = mysqli_fetch_array($sql)) {
+                //         $array['userid'] = $row['fk_userid'];
+                //         $profile = getUsersData($array['userid']);
+                //         echo "
+                //         <tr>
+                //             <td>$row[applicationid]</td>
+                //             <td>$profile[name]</td>
+                //             <td>$row[applicationdate]</td>
+                //             <td>$row[applicationtitle]</td>
+                //             <td>$row[applicationstatus]</td>
+                //             <td>
+                //                 <a class='btn btn-primary btn-sm' href='edit_user_form.php?id=$row[applicationid]'>Edit</a>
+                //                 <a class='btn btn-danger btn-sm' href='delete_user.php?id=$row[applicationid]'>Delete</a>
+                //                 <a class='btn btn-dark btn-sm' href='view_user.php?id=$row[applicationid]'>View</a>
+                //             </td>
+                //         </tr> 
+                //         ";
+                //     }
+                // } else {
+                //     echo "0 results";
+                // }
+
+                // mysqli_close($conn);
+                ?>
+            </tbody>
+        </table>
+</body>
+
+</html>
